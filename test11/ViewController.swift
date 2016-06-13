cbcc0b7a3705b643370cf95fbc657a4679b5ddcf
Sean Lin <xlinnn2@hotmail.com>
2016-06-13 16:27:54 +1000

    ssadfasdf

--- test11/ViewController.swift
+++ test11/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("This is some modification") 
+        print("This is some modification")
     }
 
     override func didReceiveMemoryWarning() {
