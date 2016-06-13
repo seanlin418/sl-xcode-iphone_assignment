1992d941702306fef1e1ad5b66a375eb5aff7bcf
Sean Lin <xlinnn2@hotmail.com>
2016-06-13 16:12:51 +1000

    Initial Commit

---
+++ test11/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test11
+//
+//  Created by Sean Lin on 13/06/2016.
+//  Copyright © 2016 au.edu.rmit.s3231048. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
